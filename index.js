4ab3a23ab8da363bfb7e5ffb328deda38f21502b
shalkauskas <igor.shalkauskas@gmail.com>
2020-07-02 13:44:57 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,36 @@
+//jshint esversion:6
+
+const express = require("express");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about-me", function(req, res){
+  res.sendFile(__dirname + "/aboutme.html");
+});
+
+app.get("/resume", function(req, res){
+  res.sendFile(__dirname + "/resume.html");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
---
+++ public/script.js
@@ -0,0 +1,34 @@
+//____________________ Arrows slide_________________________//
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('.fadein').css('display', 'none');
+$('.fadein').fadeIn(1000);
+
+});
