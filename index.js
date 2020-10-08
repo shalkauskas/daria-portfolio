82bdcc4d1718ad1b369184d90080e91f35e9b92c
shalkauskas <igor.shalkauskas@gmail.com>
2020-10-07 21:07:14 -0400

    bugfixes, nav update
    Former-commit-id: da3a4f6f8e9a26dc686d374583553e27829afcfb [formerly c84dab8445585a07515f8ee771820746edb66cf1]
    Former-commit-id: f9eb8fff362597adc863ada1831eda55f2e5f653

--- index.js
+++ index.js
@@ -9,34 +9,50 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
-  res.render("pages/index");
+  res.render("pages/index", {
+    page_name: 'index'
+  });
 });
 
 app.get("/about-me", function(req, res){
-  res.render("pages/aboutme");
+  res.render("pages/aboutme", {
+    page_name: 'aboutme'
+  });
 });
 
 app.get("/resume", function(req, res){
-  res.render("pages/resume");
+  res.render("pages/resume", {
+    page_name: 'resume'
+  });
 });
 
 app.get("/bloom-app", function(req, res){
-  res.render("pages/bloom-app");
+  res.render("pages/bloom-app", {
+    page_name: 'index'
+  });
 });
 
 app.get("/learning-seeds", function(req, res){
-  res.render("pages/learning-seeds");
+  res.render("pages/learning-seeds", {
+    page_name: 'index'
+  });
 });
 
 app.get("/venmo", function(req, res){
-  res.render("pages/venmo");
+  res.render("pages/venmo", {
+    page_name: 'index'
+  });
 });
 
 app.get("/kiki", function(req, res){
-  res.render("pages/kiki");
+  res.render("pages/kiki", {
+    page_name: 'index'
+  });
 });
 app.get("/tete-a-tete", function(req, res){
-  res.render("pages/tete-a-tete");
+  res.render("pages/tete-a-tete", {
+    page_name: 'index'
+  });
 });
 
 
--- public/script.js
+++ public/script.js
@@ -1,7 +1,4 @@
-$(".nav .nav-link").on("click", function() {
-  $(".nav").find(".active").removeClass("active");
-  $(this).addClass("active");
-});
+
 //____________________ Arrows slide_________________________//
 $(document).ready(function() {
   // Add smooth scrolling to all links
@@ -9,9 +6,7 @@ $(document).ready(function() {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
       // Store hash
       var hash = this.hash;
 
@@ -19,7 +14,6 @@ $(document).ready(function() {
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function() {
-
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
@@ -29,36 +23,21 @@ $(document).ready(function() {
 
 // ---------------------Fade In-----------------------------//
 $(document).ready(function() {
-
   /*! Fades in page on load */
   $('.fadein').css('display', 'none');
   $('.fadein').fadeIn(1000);
-
 });
-
 // ----------------------Modal---------------------------------//
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var images = document.getElementsByClassName("modal-image");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-// Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-  var img = images[i];
-  // and attach our click listener for this image.
-  img.onclick = function(evt) {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  };
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-};
+$('.modal-image').on('click', function() {
+  console.log('obj');
+  $('#img01').attr('src', $(this).attr('src'));
+  $('#myModal').show();
+  $(window).click(function(event) {
+    if (event.target.id == "myModal") {
+      $("#myModal").hide();
+    }
+  });
+});
+$(".close").on('click', (function() {
+  $("#myModal").hide();
+}));
