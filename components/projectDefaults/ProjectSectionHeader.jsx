c8f4cd29e23d3c669f2ed36da5133bfad2aa0f91
shalkauskas <igor.shalkauskas@gmail.com>
2024-03-25 16:19:16 -0400

    project section header accepts classes prop

--- components/projectDefaults/ProjectSectionHeader.jsx
+++ components/projectDefaults/ProjectSectionHeader.jsx
@@ -1,7 +1,14 @@
-export default function ProjectSectionHeader(header) {
+import { twMerge } from "tailwind-merge";
+
+export default function ProjectSectionHeader({ children, className }) {
   return (
-    <h3 className="font-bold text-center text-2xl mb-6 sm:mt-24 mt-16">
-      {header}
+    <h3
+      className={twMerge(
+        `font-bold text-center text-2xl mb-6 sm:mt-24 mt-16`,
+        className
+      )}
+    >
+      {children}
     </h3>
   );
 }
