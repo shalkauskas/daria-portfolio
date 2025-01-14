59b0c159da4d164d40f10bd2b78c36ed903e0e08
shalkauskas <igor.shalkauskas@gmail.com>
2025-01-13 22:48:33 -0500

    add mainpage data

---
+++ src/data/mainPage.ts
@@ -0,0 +1,36 @@
+export const intro = {
+  title: 'Hi, I’m Daria!',
+  statement: ' I am a Boston based UX/UI designer with a passion for understanding people’s needs.',
+  description:
+    'Meeting like-minded professionals, brainstorming new features and getting “Aha” moments together that’s what I love about my work. Let me share my latest projects with you.'
+};
+
+export const workProjects = [
+  {
+    title: 'User Research',
+    info: [
+      'Gathered quantitative and qualitative data on the metrics like usage of features, difficulty of features and overall customer satisfaction with the product.',
+      'Deep dived into the weeds of each problem and collected details via user interviews.',
+      'Synthesized the data and put together the prioritized list of must-have changes and nice-to-have enhancements.'
+    ],
+    link: ''
+  },
+  {
+    title: 'Nassan’s Place Website redesign',
+    info: [
+      'Improved site architecture based on user needs.',
+      'Enhanced usability and accessibility.',
+      'Added more prominent CTA button that boosts user’s engagement.',
+      'Reorganized resources to help users easier to scan and find information relevant to their needs.'
+    ],
+    link: ''
+  },
+  {
+    title: 'Tete-a-Tete salon Website Redesign',
+    info: [
+      'Created new responsive design that allows users to learn more about the salon using mobile phones and tablets.',
+      'The redesigned website offers more digestible price lists as well as user-oriented information.'
+    ],
+    link: ''
+  }
+];
