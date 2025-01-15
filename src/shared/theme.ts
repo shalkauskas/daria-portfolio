043c6b556c433fa9b2dd4c44c1fe6c3d39067775
shalkauskas <igor.shalkauskas@gmail.com>
2025-01-14 22:58:14 -0500

    add colors to theme

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -1,5 +1,11 @@
 export const theme = {
   colors: {
-    grey1: '#C6C6C9'
+    grey1: '#C6C6C9',
+    grey7: '#343434',
+    purple1: '#D9CBFD',
+    purple4: '#7343B2'
+  },
+  utility: {
+    pxToRem: (value: number) => `${value / 16}rem`
   }
 };
