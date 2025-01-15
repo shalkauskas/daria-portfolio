25801a36edd746431d92f6946fe8cce4b4c5c026
shalkauskas <igor.shalkauskas@gmail.com>
2025-01-14 22:38:35 -0500

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from '@emotion/react';
 import './App.css';
 import { Layout } from './components';
 import { Routing } from './navigation/Routing';
+import { theme } from './shared/theme';
 
 function App() {
   return (
-    <Layout>
-      <Routing />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Routing />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
---
+++ src/shared/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colors: {
+    grey1: '#C6C6C9'
+  }
+};
