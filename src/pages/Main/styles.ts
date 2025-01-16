cfc818d9a06c80ea1956f8ebbc68da10a6c889bf
shalkauskas <igor.shalkauskas@gmail.com>
2025-01-16 17:13:55 -0500

    main page styles

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,8 +1,14 @@
 import styled from '@emotion/styled';
 
+export const PageContainer = styled.div(({ theme }) => ({
+  background: `linear-gradient(${theme.colors.purpleBg1},${theme.colors.purpleBg1});`,
+  height: '100vh'
+}));
+
 export const WorkList = styled.div({
+  alignItems: 'center',
   display: 'flex',
   flexDirection: 'column',
   gap: '3rem',
-  margin: 'auto'
+  paddingTop: '2rem'
 });
