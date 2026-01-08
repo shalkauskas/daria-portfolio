2d88bd2c1c83bfda77dd3f5ffa17cab45755df21
shalkauskas <ihor@operix.com>
2026-01-08 14:26:42 -0500

    assets optimization

--- src/assets/main/index.ts
+++ src/assets/main/index.ts
@@ -1,4 +1,4 @@
-import aboutMe from './aboutMe_coffee.png';
+import aboutMe from './aboutMe_coffee.webp';
 import dariaBw from './daria_bw.webp';
 import gmail from './gmail_icon.svg';
 import linkedIn from './in_icon.webp';
