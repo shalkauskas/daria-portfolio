b9e17da89d47a1703f43dee3747f9efccf7a21b4
shalkauskas <ihor@operix.com>
2026-01-08 20:32:05 -0500

    scroll position fix

--- src/navigation/Routing.tsx
+++ src/navigation/Routing.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router';
 import { LikertScale, Main, Tete, UserResearch, ReviewProcess } from '@/pages';
 import { ROUTES } from './routes';
+import { ScrollToTop } from './ScrollToTop';
 
 export function Routing() {
   return (
-    <Routes>
-      <Route path={'/'} element={<Main />} />
-      <Route path={ROUTES.likertScale} element={<LikertScale />} />
-      <Route path={ROUTES.tete} element={<Tete />} />
-      <Route path={ROUTES.userResearch} element={<UserResearch />} />
-      <Route path={ROUTES.redesign} element={<ReviewProcess />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path={'/'} element={<Main />} />
+        <Route path={ROUTES.likertScale} element={<LikertScale />} />
+        <Route path={ROUTES.tete} element={<Tete />} />
+        <Route path={ROUTES.userResearch} element={<UserResearch />} />
+        <Route path={ROUTES.redesign} element={<ReviewProcess />} />
+      </Routes>
+    </>
   );
 }
